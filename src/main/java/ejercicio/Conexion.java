7f6c77fe4fa4b462b57d3f279f2a1af31939aa12
ManuelMolino <mmolmil158@g.educaand.es>
2025-05-14 12:09:42 +0200

    Inicio ejercicio jdbc, conexión a la BD

--- src/main/java/ejercicio/Conexion.java
+++ src/main/java/ejercicio/Conexion.java
@@ -1,4 +1,44 @@
 package ejercicio;
 
+import org.sqlite.SQLiteConfig;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Conexion {
+    private static Conexion conexion;
+    private Connection connection;
+
+    private Conexion() {
+        Properties properties = new Properties();
+        InputStream input = ClassLoader.getSystemClassLoader().
+                getResourceAsStream("sqlite.propierties");
+        try {
+            properties.load(input);
+            String driver = properties.getProperty("driver");
+            String db     = properties.getProperty("db");
+            String url = driver + db;
+            SQLiteConfig config = new SQLiteConfig();
+            config.enforceForeignKeys(true);
+            connection = DriverManager.getConnection(url, config.toProperties());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Conexion getInstance() {
+        if (conexion == null)
+            conexion = new Conexion();
+        return conexion;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }
--- src/main/java/ejercicio/Main.java
+++ src/main/java/ejercicio/Main.java
@@ -1,4 +1,22 @@
 package ejercicio;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
+    public static void main(String[] args) {
+        //probar que la conexión es singleton
+        Connection connection = null;
+        for (int i = 0; i < 10; i++) {
+            connection = Conexion.getInstance().getConnection();
+            System.out.println(connection);
+        }
+        connection = Conexion.getInstance().getConnection();
+        try {
+            if (connection != null)
+                connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
