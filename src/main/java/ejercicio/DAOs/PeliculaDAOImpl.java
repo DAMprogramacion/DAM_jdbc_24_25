a8e4cfed34b5c2676e3f58d25f83ccf3420564ff
ManuelMolino <mmolmil158@g.educaand.es>
2025-05-14 13:45:36 +0200

    Inicio ejercicio jdbc, inicio dao pelicula

--- src/main/java/ejercicio/DAOs/PeliculaDAOImpl.java
+++ src/main/java/ejercicio/DAOs/PeliculaDAOImpl.java
@@ -1,10 +1,15 @@
 package ejercicio.DAOs;
 
+import ejercicio.Conexion;
 import ejercicio.modelos.Pelicula;
 
+import java.sql.Connection;
+
 public class PeliculaDAOImpl implements PeliuculaDAO{
+    private Connection conn = Conexion.getInstance().getConnection();
     @Override
     public void insertarPelicula(Pelicula pelicula) {
-
+        String sql1 = " INSERT INTO video VALUES (?, ?, ?, ?, ?, ?);";
+        String sql2 = " INSERT INTO pelicula VALUES (?, ?, ?);";
     }
 }
